9168debdba32735859307af6ca518f24d94be9ee
poojaajithan <poojaajithan91@gmail.com>
2017-10-19 22:16:45 +0530

    Set up select options using AngularJS

--- MyCalculator/assets/calculator.js
+++ MyCalculator/assets/calculator.js
@@ -1,5 +1,14 @@
-/* JavaScript Document */
-
-/* Script goes here */
+(function(){
+    var app = angular.module('myCalculator',[]);
+    app.controller('CalculatorController',['$scope',function($scope){
+        
+        $scope.lumen_options = [375,600,900,1125,1600];
+        $scope.current_lumens = 600;
+        
+        
+    }]);
+    
+    
+})();
 
 
